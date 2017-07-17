7bbbf892a1343555c07962cd2d33fc8758241ef5
Hong Sung <profsung@gmail.com>
2017-07-16 20:08:07 -0500

    Initial commit

---
+++ app/src/main/java/edu/uco/hsung/m04_settings/SettingsActivity.java
@@ -0,0 +1,14 @@
+
+package edu.uco.hsung.m04_settings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_settings);
+   }
+}
